60af792f9cf947d774fc336f523f82944504e4a3
ZafirChowdhury <zafirchowdhury69@gmail.com>
2025-09-14 09:44:04 +0600

    Add base shape

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    dt = 0
+    clock = pygame.time.Clock()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     game_running = True
@@ -19,6 +22,8 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
